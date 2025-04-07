b61f640b214b67be7d3b7badf1d50e66b8ea8248
JeongSeok <ljs0429777@gmail.com>
2025-04-07 18:56:42 +0900

    init: 프로젝트 생성

---
+++ src/main/java/re/kr/icuh/icuhplatform/IcuhPlatformApplication.java
@@ -0,0 +1,13 @@
+package re.kr.icuh.icuhplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcuhPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcuhPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/re/kr/icuh/icuhplatform/IcuhPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package re.kr.icuh.icuhplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcuhPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
