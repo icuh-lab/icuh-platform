47315285b89eff56d024de6dee1b574abaae4d45
LeeJeongSeok <ljs0429777@gmail.com>
2025-05-09 15:02:07 +0900

    refactor: 대용량 파일 업로드 기능 리펙토링

--- src/main/java/re/kr/icuh/icuhplatform/controller/AttachmentApiController.java
+++ src/main/java/re/kr/icuh/icuhplatform/controller/AttachmentApiController.java
@@ -2,14 +2,11 @@ package re.kr.icuh.icuhplatform.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import re.kr.icuh.icuhplatform.service.AttachmentService;
-import re.kr.icuh.icuhplatform.util.AttachmentStore;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
@@ -20,7 +17,6 @@ import java.util.List;
 public class AttachmentApiController {
 
     private final AttachmentService attachmentService;
-    private final AttachmentStore attachmentStore;
 
     @GetMapping("/test")
     public String test() {
@@ -38,25 +34,8 @@ public class AttachmentApiController {
     @PostMapping("/large-attachment")
     public ResponseEntity<String> largeAttachments(@RequestParam("attachment") MultipartFile multipartFile) throws Exception {
 
-        try {
-            // MultipartFile을 임시 File로 변환
-            File file = File.createTempFile("temp_", multipartFile.getOriginalFilename());
-            multipartFile.transferTo(file);
+        attachmentService.uploadLargeFile(multipartFile);
 
-            // S3에 Multipart Upload 실행
-            String fileUrl = attachmentStore.uploadLargeAttachment(file);
-
-            // 임시 파일 삭제
-            if (!file.delete()) {
-                log.warn("임시 파일 삭제 실패: {}", file.getPath());
-            }
-
-            return ResponseEntity.ok(fileUrl);
-
-        } catch (Exception e) {
-            log.error("파일 업로드 실패", e);
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("파일 업로드 중 오류가 발생했습니다.");
-        }
+        return ResponseEntity.ok("파일 업로드 성공");
     }
 }
