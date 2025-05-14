28a18422a94ca0a2fdeb0c2126039c39804a262c
LeeJeongSeok <ljs0429777@gmail.com>
2025-05-14 15:29:01 +0900

    refactor: 불필요한 파일 삭제

--- src/main/java/re/kr/icuh/icuhplatform/global/exception/S3Exception.java
+++
@@ -1,8 +0,0 @@
-package re.kr.icuh.icuhplatform.global.exception;
-
-public class S3Exception extends RuntimeException{
-
-	public S3Exception(ErrorCode errorCode) {
-		super(errorCode.getMessage());
-	}
-}
