55a01fe24080f8b01d40f2ea894ac04cc524be4e
LeeJeongSeok <ljs0429777@gmail.com>
2025-05-14 14:55:51 +0900

    feat: 오류 코드 정의

--- src/main/java/re/kr/icuh/icuhplatform/global/exception/ErrorCode.java
+++ src/main/java/re/kr/icuh/icuhplatform/global/exception/ErrorCode.java
@@ -10,8 +10,21 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
 	/**
+	 * 400 Bad Request
+	 */
+	INVALID_SEARCH_PARAMETER(HttpStatus.BAD_REQUEST, "잘못된 검색 파라미터", false),
+	INVALID_REQUEST_DATA(HttpStatus.BAD_REQUEST, "유효하지 않은 데이터", false),
+
+	/**
+	 * 401 Unauthorized
+	 */
+	UNAUTHORIZED_TEMP_PASSWORD(HttpStatus.UNAUTHORIZED, "임시 비밀번호 불일치", false),
+
+	/**
 	 * 404 Not Found
 	 */
+	ARTICLE_NOT_FOUND(HttpStatus.NOT_FOUND, "게시글 없음", false),
+	FILE_NOT_FOUND(HttpStatus.NOT_FOUND, "파일 없음", false),
 	FILE_NOT_EXIST(HttpStatus.NOT_FOUND, "업로드할 파일이 존재하지 않습니다.", false),
 	FILE_READ_ERROR(HttpStatus.NOT_FOUND, "파일 읽기 중 오류가 발생했습니다.", false),
 	FILE_METADATA_CREATE_FAIL(HttpStatus.NOT_FOUND, "존재하지 않는 상품 정보입니다.", false),
@@ -20,7 +33,16 @@ public enum ErrorCode {
 
 	MULTIPART_TO_FILE_ERROR(HttpStatus.NOT_FOUND, "임시파일 변환 실패", false),
 
-	NOT_SUPPORT_EXTENSION(HttpStatus.NOT_FOUND, "지원하지 않는 파일의 확장자입니다.", false),
+	/**
+	 * 413 Payload Too Large
+	 */
+	FILE_SIZE_TOO_LARGE(HttpStatus.PAYLOAD_TOO_LARGE, "업로드 파일 크기 초과", false),
+
+
+	/**
+	 * 415 Unsupported Media Type
+	 */
+	UNSUPPORTED_FILE_TYPE(HttpStatus.UNSUPPORTED_MEDIA_TYPE, "지원하지 않는 파일 확장자", false),
 
 	/**
 	 * 500 Internal Server Error
