209e3d3fc9895fc75c9138d6a0c47ce86ceeeddc
JeongSeok <ljs0429777@gmail.com>
2025-05-14 14:08:33 +0900

    refactor: 패키지 변경으로 인한 import문 수정

--- src/main/java/re/kr/icuh/icuhplatform/service/S3FileUploader.java
+++ src/main/java/re/kr/icuh/icuhplatform/service/S3FileUploader.java
@@ -7,7 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
-import re.kr.icuh.icuhplatform.common.util.FileUtils;
+import re.kr.icuh.icuhplatform.global.util.FileUtils;
 import re.kr.icuh.icuhplatform.domain.FileMetadata;
 import re.kr.icuh.icuhplatform.dto.CreateAttachmentDto;
 
--- src/test/java/re/kr/icuh/icuhplatform/util/S3FileUploaderTest.java
+++ src/test/java/re/kr/icuh/icuhplatform/util/S3FileUploaderTest.java
@@ -11,7 +11,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.multipart.MultipartFile;
-import re.kr.icuh.icuhplatform.common.util.FileUtils;
+import re.kr.icuh.icuhplatform.global.util.FileUtils;
 import re.kr.icuh.icuhplatform.dto.CreateAttachmentDto;
 import re.kr.icuh.icuhplatform.service.S3FileUploader;
 
