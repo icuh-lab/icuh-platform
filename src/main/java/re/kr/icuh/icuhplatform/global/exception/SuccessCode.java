f732a62db4ad198504506e142c95ccc077b7ebe3
LeeJeongSeok <ljs0429777@gmail.com>
2025-05-14 14:57:21 +0900

    feat: 성공 코드 정의

--- src/main/java/re/kr/icuh/icuhplatform/global/exception/SuccessCode.java
+++ src/main/java/re/kr/icuh/icuhplatform/global/exception/SuccessCode.java
@@ -12,15 +12,15 @@ public enum SuccessCode {
 	/**
 	 * 200 OK
 	 */
-	PRODUCT_LIST_GET_SUCCESS(HttpStatus.OK, "상품 리스트 조회 성공", true),
-	PRODUCT_GET_SUCCESS(HttpStatus.OK, "상품 조회 성공", true),
-	PRODUCT_UPDATE_SUCCESS(HttpStatus.OK, "상품 수정 성공", true),
-	PRODUCT_DELETE_SUCCESS(HttpStatus.OK, "상품 삭제 성공", true),
+	ARTICLE_LIST_GET_SUCCESS(HttpStatus.OK, "게시글 리스트 조회 성공", true),
+	ARTICLE_GET_SUCCESS(HttpStatus.OK, "게시글 조회 성공", true),
+	ARTICLE_UPDATE_SUCCESS(HttpStatus.OK, "게시글 수정 성공", true),
+	ARTICLE_DELETE_SUCCESS(HttpStatus.OK, "게시글 삭제 성공", true),
 
 	/**
 	 * 201 Createed
 	 */
-	PRODUCT_CREATE_SUCCESS(HttpStatus.CREATED, "상품 생성 성공", true),
+	ARTICLE_CREATE_SUCCESS(HttpStatus.CREATED, "게시글 생성 완료", true),
 	;
 
 	private final HttpStatus httpStatus;
