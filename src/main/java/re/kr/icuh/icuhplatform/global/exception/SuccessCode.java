1dd28b62dbe7ef78891a111e6a152bd3239cd341
LeeJeongSeok <ljs0429777@gmail.com>
2025-05-14 15:11:02 +0900

    refactor: 성공 코드 수정

--- src/main/java/re/kr/icuh/icuhplatform/global/exception/SuccessCode.java
+++ src/main/java/re/kr/icuh/icuhplatform/global/exception/SuccessCode.java
@@ -1,29 +1,31 @@
 package re.kr.icuh.icuhplatform.global.exception;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum SuccessCode {
 
 	/**
 	 * 200 OK
 	 */
-	ARTICLE_LIST_GET_SUCCESS(HttpStatus.OK, "게시글 리스트 조회 성공", true),
-	ARTICLE_GET_SUCCESS(HttpStatus.OK, "게시글 조회 성공", true),
-	ARTICLE_UPDATE_SUCCESS(HttpStatus.OK, "게시글 수정 성공", true),
-	ARTICLE_DELETE_SUCCESS(HttpStatus.OK, "게시글 삭제 성공", true),
+	ARTICLE_LIST_GET_SUCCESS(HttpStatus.OK, "ARTICLE_LIST_GET_SUCCESS", "게시글 리스트 조회 성공"),
+	ARTICLE_GET_SUCCESS(HttpStatus.OK, "ARTICLE_GET_SUCCESS", "게시글 조회 성공"),
+	ARTICLE_UPDATE_SUCCESS(HttpStatus.OK, "ARTICLE_UPDATE_SUCCESS", "게시글 수정 성공"),
+	ARTICLE_DELETE_SUCCESS(HttpStatus.OK, "ARTICLE_DELETE_SUCCESS", "게시글 삭제 성공"),
 
 	/**
 	 * 201 Createed
 	 */
-	ARTICLE_CREATE_SUCCESS(HttpStatus.CREATED, "게시글 생성 완료", true),
-	;
+	ARTICLE_CREATE_SUCCESS(HttpStatus.CREATED, "ARTICLE_CREATE_SUCCESS", "게시글 생성 완료");
 
-	private final HttpStatus httpStatus;
+	private final HttpStatus status;
+	private final String code;
 	private final String message;
-	private final boolean isSuccess;
+
+	SuccessCode(HttpStatus status, String code, String message) {
+		this.status = status;
+		this.code = code;
+		this.message = message;
+	}
 }
