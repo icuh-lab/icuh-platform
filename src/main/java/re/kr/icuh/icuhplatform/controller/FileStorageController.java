dac320897d9d12dfabb81b433437e7197722d88b
LeeJeongSeok <ljs0429777@gmail.com>
2025-05-20 16:53:37 +0900

    refactor: 불필요한 코드 제거 및 게시글 생성 API 추가

--- src/main/java/re/kr/icuh/icuhplatform/controller/FileStorageController.java
+++ src/main/java/re/kr/icuh/icuhplatform/controller/FileStorageController.java
@@ -1,14 +1,14 @@
 package re.kr.icuh.icuhplatform.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.tomcat.util.http.fileupload.FileUpload;
-import org.apache.tomcat.util.http.fileupload.FileUploadException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import re.kr.icuh.icuhplatform.service.FileStorageService;
+import re.kr.icuh.icuhplatform.dto.article.CreateArticleRequest;
+import re.kr.icuh.icuhplatform.service.ArticleService;
 
 import java.io.IOException;
 import java.util.List;
@@ -19,27 +19,14 @@ import java.util.List;
 @RequiredArgsConstructor
 public class FileStorageController {
 
-    private final FileStorageService fileStorageService;
+    private final ArticleService articleService;
 
-    @GetMapping("/test")
-    @ResponseStatus(value = HttpStatus.OK)
-    public String test() {
-        return "<h1> 안녕하세요 가뭄 정보 빅데이터 플랫폼에 오신 것을 환영합니다. </h1>";
-    }
-
-    @PostMapping("/attachments")
+    @PostMapping("/articles")
     @ResponseStatus(value = HttpStatus.CREATED)
-    public ResponseEntity<String> attachments(@RequestPart("attachment") List<MultipartFile> file) throws IOException {
-        fileStorageService.createAttachment(file);
+    public ResponseEntity<String> createArticle(@RequestPart @Valid CreateArticleRequest request, @RequestPart List<MultipartFile> files) throws IOException {
+
+        articleService.createArticle(request, files);
 
         return ResponseEntity.ok("파일 업로드 성공");
-    }
-
-    @PostMapping("/large-attachment")
-    @ResponseStatus(value = HttpStatus.CREATED)
-    public ResponseEntity<String> largeAttachments(@RequestParam("attachment") MultipartFile multipartFile) throws Exception {
-        fileStorageService.uploadLargeFile(multipartFile);
-
-        return ResponseEntity.ok("대용량 파일 업로드 성공");
     }
 }
