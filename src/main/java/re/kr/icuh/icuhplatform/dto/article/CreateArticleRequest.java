ad14908b2ab6a1b805364d782d2c23024d4beb8d
LeeJeongSeok <ljs0429777@gmail.com>
2025-05-20 16:51:05 +0900

    feat: Valid 애노테이션 추가

--- src/main/java/re/kr/icuh/icuhplatform/dto/article/CreateArticleRequest.java
+++ src/main/java/re/kr/icuh/icuhplatform/dto/article/CreateArticleRequest.java
@@ -1,12 +1,14 @@
 package re.kr.icuh.icuhplatform.dto.article;
 
+import jakarta.validation.constraints.NotNull;
+
 public record CreateArticleRequest(
-        String title,
-        String description,
-        String author,
-        String authorOrganization,
-        String department,
-        String tempPassword,
-        Long classificationId,
-        Long serviceTypeId
+        @NotNull String title,
+        @NotNull String description,
+        @NotNull String author,
+        @NotNull String authorOrganization,
+        @NotNull String department,
+        @NotNull String tempPassword,
+        @NotNull Long classificationId,
+        @NotNull Long serviceTypeId
 ) {}
