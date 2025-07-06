7fc077b776941d0106a7d67d04bae04351877fba
JeongSeok <ljs0429777@gmail.com>
2025-07-06 17:14:18 +0900

    refactor: classification -> documentType 으로 변경, serviceType -> subjectDomain 으로 변경

---
+++ src/main/java/re/kr/icuh/icuhplatform/dto/subjectdomain/SubjectDomainResponse.java
@@ -0,0 +1,15 @@
+package re.kr.icuh.icuhplatform.dto.subjectdomain;
+
+import re.kr.icuh.icuhplatform.domain.SubjectDomain;
+
+public record SubjectDomainResponse(
+        Long id,
+        String name
+) {
+    public static SubjectDomainResponse fromEntity(SubjectDomain subjectDomain) {
+        return new SubjectDomainResponse(
+                subjectDomain.getId(),
+                subjectDomain.getName()
+        );
+    }
+}
