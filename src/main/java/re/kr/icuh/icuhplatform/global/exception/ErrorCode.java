364b1f5b284a4844b9181079b4f8d9ed8dd05265
LeeJeongSeok <ljs0429777@gmail.com>
2025-07-07 15:24:30 +0900

    feat: classification -> document_type으로 변경, service_type -> subject_domain으로 변경

--- src/main/java/re/kr/icuh/icuhplatform/global/exception/ErrorCode.java
+++ src/main/java/re/kr/icuh/icuhplatform/global/exception/ErrorCode.java
@@ -11,8 +11,8 @@ public enum ErrorCode {
 	 */
 	INVALID_INPUT(HttpStatus.BAD_REQUEST, "INVALID_INPUT", "유효하지 않은 입력값입니다."),
 	MISSING_REQUIRED_FIELD(HttpStatus.BAD_REQUEST, "MISSING_REQUIRED_FIELD", "필수 필드가 누락되었습니다."),
-	CLASSIFICATION_NOT_FOUND(HttpStatus.BAD_REQUEST, "CLASSIFICATION_NOT_FOUND", "분류체계가 아닙니다."),
-	SERVICE_TYPE_NOT_FOUND(HttpStatus.BAD_REQUEST, "SERVICE_TYPE_NOT_FOUND", "서비스 타입이 아닙니다."),
+	DOCUMENT_TYPE_NOT_FOUND(HttpStatus.BAD_REQUEST, "DOCUMENT_TYPE_NOT_FOUND", "문서 성격을 찾을 수 없습니다. 관리자에게 문의해주세요."),
+	SUBJECT_DOMAIN_NOT_FOUND(HttpStatus.BAD_REQUEST, "SUBJECT_DOMAIN_NOT_FOUND", "주제 영역을 찾을 수 없습니다. 관리자에게 문의해주세요."),
 	/**
 	 * 401 Unauthorized
 	 */
