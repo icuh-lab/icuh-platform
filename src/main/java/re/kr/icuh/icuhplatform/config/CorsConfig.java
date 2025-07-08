ce20991e808efea773aa0f87463c858a472d193c
LeeJeongSeok <ljs0429777@gmail.com>
2025-07-08 15:08:49 +0900

    feat: cors 정책 설정

---
+++ src/main/java/re/kr/icuh/icuhplatform/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package re.kr.icuh.icuhplatform.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173") // 허용할 출처
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
+                .allowedHeaders("*") // 허용할 헤더
+                .allowCredentials(true); // 인증 정보 허용 (선택 사항)
+    }
+}
