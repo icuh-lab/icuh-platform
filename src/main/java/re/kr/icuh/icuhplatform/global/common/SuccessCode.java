e76aa0c117f78739abe62ecdd7ac19c199b339f1
LeeJeongSeok <ljs0429777@gmail.com>
2025-08-05 07:53:41 +0900

    feat: SuccessCode 값 추가 - 게시글 수정 요청 완료

--- src/main/java/re/kr/icuh/icuhplatform/global/common/SuccessCode.java
+++ src/main/java/re/kr/icuh/icuhplatform/global/common/SuccessCode.java
@@ -11,6 +11,7 @@ public enum SuccessCode {
 	 */
 	ARTICLE_LIST_GET_SUCCESS(HttpStatus.OK, "ARTICLE_LIST_GET_SUCCESS", "게시글 리스트 조회 성공"),
 	ARTICLE_GET_SUCCESS(HttpStatus.OK, "ARTICLE_GET_SUCCESS", "게시글 조회 성공"),
+	ARTICLE_UPDATE_PENDING(HttpStatus.OK, "ARTICLE_UPDATE_PENDING", "게시글 수정 요청 완료"),
 	ARTICLE_UPDATE_SUCCESS(HttpStatus.OK, "ARTICLE_UPDATE_SUCCESS", "게시글 수정 성공"),
 	ARTICLE_DELETE_SUCCESS(HttpStatus.OK, "ARTICLE_DELETE_SUCCESS", "게시글 삭제 성공"),
 
