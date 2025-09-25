126820221d3ea88a7a0d23d999eb83a82ee62214
jeongseok <ljs0429777@gmail.com>
2025-09-25 11:34:45 +0900

    refactor: Attchment 파일 제거

--- src/main/java/re/kr/icuh/icuhplatform/repository/FileStorageRepository.java
+++
@@ -1,9 +0,0 @@
-package re.kr.icuh.icuhplatform.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import re.kr.icuh.icuhplatform.domain.Attachment;
-
-@Repository
-public interface FileStorageRepository extends JpaRepository<Attachment,Long> {
-}
