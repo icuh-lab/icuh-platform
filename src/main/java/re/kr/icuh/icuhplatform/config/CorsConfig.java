41c5a15c74159eeeb21242f94e7977c007c25e89
jeongseok <ljs0429777@gmail.com>
2025-11-04 15:21:56 +0900

    chore: CORS origins 소스 환경변수처리

--- src/main/java/re/kr/icuh/icuhplatform/config/CorsConfig.java
+++ src/main/java/re/kr/icuh/icuhplatform/config/CorsConfig.java
@@ -1,15 +1,20 @@
 package re.kr.icuh.icuhplatform.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
+    @Value("${cors.allowed.origins}")
+    private String allowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://infradna.io.kr") // 허용할 출처
+                .allowedOrigins(allowedOrigins) // 허용할 출처
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 허용할 헤더
                 .allowCredentials(true); // 인증 정보 허용 (선택 사항)
