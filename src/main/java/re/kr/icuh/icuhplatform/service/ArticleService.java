2c2875be1e5a03044acfd12f4249585a5887943d
jeongseok <ljs0429777@gmail.com>
2026-01-05 19:39:20 +0900

    refactor: 중복 메소드 private 메소드로 분리 및 메소드명 리펙토

--- src/main/java/re/kr/icuh/icuhplatform/service/ArticleService.java
+++ src/main/java/re/kr/icuh/icuhplatform/service/ArticleService.java
@@ -1,7 +1,6 @@
 package re.kr.icuh.icuhplatform.service;
 
 import com.querydsl.jpa.impl.JPAQuery;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -73,53 +72,43 @@ public class ArticleService {
 
     @Transactional
     public ArticleDetailResponse findArticleById(Long id) {
-        if (!articleRepository.findById(id).isPresent()) {
-            throw new BusinessException(ErrorCode.ARTICLE_NOT_FOUND);
-        }
+        Article savedArticle = findSavedArticle(id);
+        savedArticle.increaseViews();
 
-        Article article = articleRepository.findById(id).get();
-        article.increaseViews();
-
-        return ArticleDetailResponse.of(article);
+        return ArticleDetailResponse.of(savedArticle);
     }
 
     @Transactional
     public ArticleDetailResponse modifyArticleStatus(Long id, ModifyArticleStatusRequest request) {
-        Article article = articleRepository.findById(id)
-                .orElseThrow(() -> new BusinessException(ErrorCode.ARTICLE_NOT_FOUND));
-
-        if (!article.validatePassword(request.password())) {
-            throw new BusinessException(ErrorCode.INVALID_PASSWORD);
-        }
+        Article article = findSavedArticle(id);
+        validatePassword(article, request.password());
 
         return ArticleDetailResponse.of(article);
     }
 
     @Transactional
-    public Long updateArticle(Long id, @Valid UpdateArticleRequest request) {
-        DocumentType documentType = validateDocumentType(request.documentTypeId());
-        SubjectDomain subjectDomain = validateSubjectDomain(request.subjectDomainId());
-
-        // 기존에 작성되어 있던 내용은 그대로 가져오고, 새로 작성되는 내용만 덮어쓴다. 패스워드는 그전에 사용했던 값을 그대로 사용
-        Article savedArticle = articleRepository.findById(id)
-                .orElseThrow(() -> new BusinessException(ErrorCode.ARTICLE_NOT_FOUND));
-
-        // UpdateArticleRequest 객체를 json 형식으로 컬럼에 집어넣어야함
-        savedArticle.setPendingUpdate(request);
-
-        return savedArticle.getId();
+    public void updateArticle(Long id, UpdateArticleRequest request) {
+        Article savedArticle = findSavedArticle(id);
+        savedArticle.updateContent(request);
     }
 
     @Transactional
     public void deleteArticle(Long articleId, DeleteArticleRequest request) {
-        Article article = articleRepository.findById(articleId)
-                .orElseThrow(() -> new BusinessException(ErrorCode.ARTICLE_NOT_FOUND));
+        Article savedArticle = findSavedArticle(articleId);
+        validatePassword(savedArticle, request.password());
 
-        if (!article.validatePassword(request.password())) {
+        savedArticle.delete();
+    }
+
+    private static void validatePassword(Article article, String password) {
+        if (!article.validatePassword(password)) {
             throw new BusinessException(ErrorCode.INVALID_PASSWORD);
         }
+    }
 
-        article.softDelete();
+    private Article findSavedArticle(Long articleId) {
+        return articleRepository.findById(articleId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.ARTICLE_NOT_FOUND));
     }
 
     private DocumentType validateDocumentType(Long documentTypeId) {
